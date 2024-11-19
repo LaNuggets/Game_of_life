b7cf5cd2e7937845b8d644ae382d2470369f42d3
LaNuggets <aurelien.dugast44@gmail.com>
2024-11-19 18:30:03 +0100

    change generate grid unf

--- generate_grid.py
+++ generate_grid.py
@@ -1,10 +1,9 @@
 import random
 
 def generate_grid():
-    length = input("enter length: ")
-    width = input("enter width: ")
+    grid_size = input("Enter the grid size: ")
     components = [0, 1]
 
-    grid = [[random.choice(components) for _ in range(int(length))] for _ in range(int(width))]
+    grid = [[random.choice(components) for _ in range(int(grid_size))] for _ in range(int(grid_size))]
 
     return grid
