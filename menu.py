92d7adfd5d621df396ae2c326b2be66b96be35dd
daurelie <aurelien.dugast@ynov.com>
2024-11-19 16:32:45 +0100

    debug

---
+++ menu.py
@@ -0,0 +1,7 @@
+import os
+
+def menu():
+    os.system('clear')
+    print("Welcome to the Game of Life !\n")
+    print('This is an auto generate game base on your choices \n')
+    print('At any moment press\'q\' to exit')
