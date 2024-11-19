123d3a46e0b4754a9c20b8844f5a3582764e4a68
daurelie <aurelien.dugast@ynov.com>
2024-11-19 17:00:40 +0100

    add turn func

--- menu.py
+++ menu.py
@@ -6,3 +6,18 @@ def menu():
     print("Welcome to the Game of Life !\n")
     print('This is an auto generate game base on your choices \n')
     print('At any moment press\'q\' to exit')
+
+
+
+def turn():
+    user_responce = ""
+    while(user_responce != 'q' or user_responce != 'enter'):
+        user_responce = input('Do you want to continue, press "Enter" to continue or "q" to finish: ')
+        if user_responce == 'q':
+            exit(1)
+        elif user_responce == '\n':
+            continue
+        else:
+            print('Wrong key pressed. Please use "Enter" or "q": ')
+
+turn()
